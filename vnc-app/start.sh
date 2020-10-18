2be988ef129509f121a0c26bde4b3dffa916a756
petrnetusil <petr.netusil@gmail.com>
2020-10-18 21:36:03 +0200

    change vnc implementation

--- vnc-app/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z ${VNC_PASS} ]; then
-  echo "STARTING VNC WITHOUT PASSWORD"
-  supervisord -c /etc/supervisor/supervisord_np.conf  
-else
-  echo "STARTING VNC WITH PASSWORD"
-  supervisord -c /etc/supervisor/supervisord.conf
-fi
